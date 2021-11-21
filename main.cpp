3ae35665f36dc35d75bc4b557d6a19513f985881
Jaspier <dungoallen349@gmail.com>
2021-11-21 23:54:40 +0000

    Moving the map with WASD keys

--- main.cpp
+++ main.cpp
@@ -1,30 +1,36 @@
 #include "raylib.h"
+#include "raymath.h"
+
 int main()
 {
-    // Window dimensions
     const int windowWidth{384};
     const int windowHeight{384};
-
-    // Initialize the window
     InitWindow(windowWidth, windowHeight, "Jaspier Monster Slayer!");
-    
+
     Texture2D map = LoadTexture("./nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // draw the map
-        Vector2 mapPos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+        
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        // stop drawing
         EndDrawing();
     }
-
-    UnloadTexture(map);
     CloseWindow();
 }
